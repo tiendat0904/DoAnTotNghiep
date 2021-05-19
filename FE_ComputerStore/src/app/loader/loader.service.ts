f671380449c1827551820a0d99065c24a373b91f
Tiến Đạt <tiendat09041999@gmail.com>
2021-05-19 22:40:38 +0700

    add loading snipper

--- FE_ComputerStore/src/app/loader/loader.service.ts
+++ FE_ComputerStore/src/app/loader/loader.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
-  isLoading = new Subject<boolean>();
+  public isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   constructor() { }
 
   show() {
